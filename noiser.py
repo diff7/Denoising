8dec34df79fcf9a98860eaf3afd26782e2a869c8
Egorsv <geksut@gmail.com>
2020-09-29 11:20:23 +0000

    auto-commit from @9c196e37ca1d on Tue Sep 29 11:20:20 UTC 2020

---
+++ noiser.py
@@ -0,0 +1,117 @@
+import os
+import torch 
+import random
+from multiprocess import get_context
+import torchaudio as ta
+from tqdm import tqdm
+from omegaconf import OmegaConf
+
+def read_A_file(file):
+    waveform, sr = ta.load(file)
+    return waveform
+
+def align(waveform, new_length):
+    if len(waveform[0]) >= new_length: new_waveform = waveform[:,:new_length]
+    if len(waveform[0]) < new_length:
+        m, l = new_length//len(waveform[0]), new_length%len(waveform[0])
+        new_waveform = [waveform]*m
+        new_waveform.append(waveform[:,:l])
+        new_waveform = torch.cat(new_waveform, 1)
+    return new_waveform
+
+
+def read_n_align(file, new_length, resample):
+    waveform = read_A_file(file)
+    waveform = resample(waveform)
+    return align(waveform, new_length)
+
+def read_random_batch(all_files, 
+                      resample, 
+                      batch_size, 
+                      workers, 
+                      new_length):
+    
+    files_batch = random.sample(all_files, batch_size)
+    new_length = [new_length]*batch_size
+    resample = [resample]*batch_size
+    with get_context("spawn").Pool(workers) as p:
+        files = p.starmap(read_n_align, zip(files_batch, new_length, resample))
+    return torch.cat(files, 0)
+
+
+rms = lambda x: torch.pow(torch.pow(x, 2).mean(1),0.5)
+
+def snr(signal, noise):
+    r_s = rms(signal)
+    r_n = rms(noise)
+    return 20*torch.log10(r_s/r_n)
+
+def get_noise_scale(signal, snr):
+    r_s = rms(signal)
+    snr_ten = torch.pow(10, torch.true_divide(snr,10))
+    return r_s/torch.pow(snr_ten,0.5)
+
+def get_random_noise_snr(signal, snr):
+    scale = get_noise_scale(signal, snr)
+    size = signal.shape[1]
+    return torch.randn(size=(1,size))*scale.view(-1,1)
+
+def get_snr_scaled_noise(signal, noise, snr):
+    scale = get_noise_scale(signal, snr)
+    r_n = rms(noise)
+    return noise*torch.true_divide(scale,r_n).view(-1,1)
+
+
+def make_noisy_file(clean_file_path, cfg, noise_files, resample):
+    output_file = open(cfg.save_records,'a')
+    waveform = read_A_file(clean_file_path)
+    file_name = os.path.join(cfg.result_dir, 
+                             clean_file_path.split('/')[-1].split('.')[0])
+
+    snr = random.randint(cfg.snr_min,cfg.snr_max)
+    file_name  = file_name + f'_snr{snr}'
+    # get random sounds files and align them with source file len
+    base_len = len(waveform[0])
+    noises_batch = read_random_batch(noise_files, 
+                                     resample,
+                                     batch_size=cfg.num_copies, 
+                                     workers=cfg.workers, 
+                                     new_length=base_len)
+    
+    noised = get_snr_scaled_noise(waveform, noises_batch, snr) + waveform
+    if random.random() > 0.5:
+        snr = random.randint(cfg.snr_min,cfg.snr_max)
+        noised = noised + get_random_noise_snr(noised, snr)
+        file_name  = file_name + f'_snr{snr}'
+
+    def save_file(file, idx):
+        new_name = file_name+f'_idx{idx}.wav'
+        ta.save(new_name, file, sample_rate=cfg.source_sr)
+        output_file.write(f'{clean_file_path} {new_name}\n')
+
+    with get_context("spawn").Pool(cfg.workers) as p:
+            files = p.starmap(save_file, [(noised[i], i) for i in range(cfg.num_copies)])
+    output_file.close()
+
+if __name__ == "__main__":
+    cfg = OmegaConf.load("noise_conf.yaml")
+    os.makedirs(cfg.result_dir, exist_ok=True)
+    clean_files = cfg.clean_files_txt
+    noise_files = cfg.noise_files_txt
+    
+    with open(noise_files, 'r') as f:
+        noise_files = f.read().split('\n')
+
+    with open(clean_files, 'r') as f:
+        clean_files = f.read().split('\n')
+    
+    
+    
+    # resample to match noise_sr == clean_sr
+    resample  = ta.transforms.Resample(orig_freq= cfg.noise_sr, 
+                                       new_freq = cfg.source_sr, 
+                                       resampling_method = 'sinc_interpolation')
+    
+        
+    for f in tqdm(clean_files):
+        make_noisy_file(f, cfg, noise_files, resample)
